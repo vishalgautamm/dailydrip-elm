b3649d97528a045b526e8a8beebb9f8d7a16b6a5
Yong Bakos <ybakos@humanoriented.com>
2017-04-08 08:24:12 -0700

    004.4: Prepare copy of counter app.

---
+++ counter2/Main.elm
@@ -0,0 +1,51 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (onClick)
+
+
+type alias Model =
+    { counter : Int
+    , decrements : Int
+    , increments : Int
+    }
+
+
+initialModel : Model
+initialModel =
+    Model 0 0 0
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | counter = model.counter + 1, increments = model.increments + 1 }
+
+        Decrement ->
+            { model | counter = model.counter - 1, decrements = model.decrements + 1 }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model.counter) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , h3 [] [ text ("# of increments: " ++ (toString model.increments)) ]
+        , h3 [] [ text ("# of decrements: " ++ (toString model.decrements)) ]
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
